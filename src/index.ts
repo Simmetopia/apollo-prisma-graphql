19ca53ed8dded113e2923a17657a3a466e0b4b1b
EhabItMinds <ehb@it-minds.dk>
2023-09-14 15:30:20 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { PrismaClient } from '@prisma/client';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
 import { GraphQLSchema } from 'graphql';
+import { PubSub } from 'graphql-subscriptions';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import http from 'http';
 import { makeSchema } from 'nexus';
 import { $settings } from 'nexus-prisma';
 import path, { join } from 'path';
+import { WebSocketServer } from 'ws';
 
 import { VerifyToken } from './middleware';
 import * as types from './types';
-
 const shema = makeSchema({
   types,
   contextType: {
@@ -20,9 +27,40 @@ const shema = makeSchema({
     schema: join(__dirname, 'schema.graphql'), // 3
   },
 });
+export const pubsub = new PubSub();
+
 const prisma_client = new PrismaClient();
+const app = express();
+const httpServer = http.createServer(app);
+// Creating the WebSocket server
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
+});
+
+const serverCleanup = useServer(
+  {
+    schema: shema,
+    context: async () => {
+      return { db: prisma_client };
+    },
+  },
+  wsServer,
+);
 const server = new ApolloServer({
   schema: shema as unknown as GraphQLSchema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
 });
 
 $settings({
@@ -30,15 +68,24 @@ $settings({
 });
 const port = Number(process.env.PORT) || 4000;
 
-startStandaloneServer(server, {
-  listen: { port: port },
-  context: async (request) => {
-    return {
-      ...request,
-      db: prisma_client,
-      user: await VerifyToken(request.req, prisma_client),
-    };
-  },
-}).then((app) => {
-  console.log(`🚀  Server ready at: ${app.url}`);
+server.start().then(() => {
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: async (request) => {
+        return {
+          ...request,
+          db: prisma_client,
+          user: await VerifyToken(request.req, prisma_client),
+        };
+      },
+    }),
+  );
+
+  // Modified server startup
+  httpServer.listen({ port }, () => {
+    console.log('somethign serverlike happened');
+  });
 });
